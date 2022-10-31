41dde6181f0dab1a78e459cafd01e82f0ddc454c
Xavier Fenton <xavierfenton99@gmail.com>
2022-11-01 10:32:38 +1300

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css'
 import Docs from './components/Docs'
+import { Routes, Route } from 'react-router-dom'
 import { app, database } from './firebaseConfig'
+import EditDocs from './components/EditDocs'
 
 function App() {
-  return <Docs database={database} app={app} />
+  return (
+    <Routes>
+      <Route path="/" element={<Docs database={database} />} />
+      <Route path="/editDocs/:id" element={<EditDocs database={database} />} />
+    </Routes>
+  )
 }
 
 export default App
