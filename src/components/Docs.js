0bdd541f3239d41787daa0d0bae1b5a68fc67641
Xavier Fenton <xavierfenton99@gmail.com>
2022-11-01 09:51:12 +1300

    set up posting data

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,9 +1,26 @@
 import React, { useState } from 'react'
 import Modal from './Modal'
-
+import { addDoc, collection } from 'firebase/firestore'
 export default function Docs({ database }) {
+  const [title, setTitle] = useState('')
   const [open, setOpen] = React.useState(false)
   const handleOpen = () => setOpen(true)
+
+  //database
+
+  const collectionRef = collection(database, 'docsData')
+
+  const addData = () => {
+    addDoc(collectionRef, {
+      title: title,
+    })
+      .then(() => {
+        alert('Data Added')
+      })
+      .catch(() => {
+        alert('Cannot add data')
+      })
+  }
   return (
     <div className="docs-main">
       <h1>Docs Clone</h1>
@@ -12,7 +29,13 @@ export default function Docs({ database }) {
         Add a Document
       </button>
 
-      <Modal open={open} setOpen={setOpen} />
+      <Modal
+        open={open}
+        setOpen={setOpen}
+        title={title}
+        setTitle={setTitle}
+        addData={addData}
+      />
     </div>
   )
 }
