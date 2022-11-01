58ae49ce428efc6b32fd6a2308f07f34da564f8a
Xavier Fenton <xavierfenton99@gmail.com>
2022-11-01 15:33:35 +1300

    import functions from firestore

--- src/components/EditDocs.js
+++ src/components/EditDocs.js
@@ -1,19 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 
+//import firebase
+import { updateDoc, collection, doc } from 'firebase/firestore'
+import { database } from '../firebaseConfig'
+
 //
-export default function EditDocs() {
+export default function EditDocs(id) {
   const [docsDesc, setDocsDesc] = useState('')
   //getQuillData
   const getQuillData = (value) => {
     setDocsDesc(value)
   }
-  let params = useParams()
+  let params = useParams(id)
+
+  //updateDocsData
+  const collectionRef = collection(database, 'docsData')
+
+  //useEff
+  useEffect(() => {
+    const updateDocsData = setTimeout(() => {
+      const document = doc(collectionRef, params.id)
+      updateDoc(document, {
+        docsDesc: docsDesc,
+      })
+        .then(() => {
+          alert('Saved')
+        })
+        .catch(() => {
+          alert('Cannot Save')
+        })
+    }, 5000)
+    return () => clearTimeout(updateDocsData)
+  }, [docsDesc])
 
   return (
-    <div>
+    <div key={docsDesc}>
       <h1>EditDocs</h1>
       <ReactQuill value={docsDesc} onChange={getQuillData} />
     </div>
